ace4cb49357e36c1fa05c29284524eee4d333006
Benjamin Cohen-Wang <ben.cohenwang@gmail.com>
2016-05-19 10:09:29 -0700

    Removed control for demonstration

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -5,26 +5,7 @@ public class Controller {
 	public static boolean visual = true;
 	public static Drivetrain drivetrain;
 	
-	public static double integral = 0;
-	
 	public static void updateDrivetrain() {
-		PID(1000, 1, 187);
-	}
-	
-	public static void PID(double p, double i, double d) {
-		double proportional = drivetrain.getTarget() - drivetrain.getPosition();
-		double derivative = -drivetrain.getVelocity();
-		integral += drivetrain.getPosition() / Constants.updateRate;
 		
-		drivetrain.drive(p*proportional + i*integral + d*derivative);
-	}
-	
-	public static void bangBang(double speed) {
-		if(drivetrain.getPosition() > drivetrain.getTarget()) {
-			drivetrain.drive(-speed);
-		}
-		if(drivetrain.getPosition() < drivetrain.getTarget()) {
-			drivetrain.drive(speed);
-		}
 	}
 }
